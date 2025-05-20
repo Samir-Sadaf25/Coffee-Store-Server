3ec51f3bac10c8a1b2ba8301a4357432f66d3fef
Samir-Sadaf25 <samirsadaf.25@gmail.com>
2025-05-20 14:39:32 +0600

    Add DELETE endpoint to remove coffee from MongoDB when user clicks delete button

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.listen(port, () => {
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vvycnhh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -41,7 +41,12 @@ async function run() {
             const result = await coffeeCollection.find().toArray();
             res.send(result);
         })
-
+        app.delete('/coffees/:id',async(req,res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await coffeeCollection.deleteOne(query);
+            res.send(result);
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
